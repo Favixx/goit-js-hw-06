1d0fd64107dfd3ff623edd494b35ebf7689d3ae1
Favixx <bogdandom52@gmail.com>
2023-04-24 20:01:57 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,11 @@
 //Частина 1
 const categoriesList = document.querySelectorAll('ul#categories li.item');
-console.log(`Кількість категорій: ${categoriesList.length}`);
+console.log(`Number of categories: ${categoriesList.length}`);
 
 //Частина 2
 categoriesList.forEach((item)=>{
     const categoryTitle = item.querySelector("h2").textContent;
     const categoryElements = item.querySelectorAll("li").length
-    console.log(`Категорія: ${categoryTitle}, Елементів в категорії ${categoryElements}`);
+    console.log(`Category: ${categoryTitle}`)
+    console.log(`Elements ${categoryElements}`);
 })
