527eb831adc6395b5920bec33f54fe95d1a58b33
Favixx <bogdandom52@gmail.com>
2023-04-26 02:31:26 +0300

    task10 is ready

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,25 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const boxesDiv = document.querySelector("#boxes")
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const amount = document.querySelector('[type="number"]')
+let sizes = 30;
+function creatingHandler(){
+  for (let i = 0; i<amount.value; i+=1){
+   let item = document.createElement("div")
+   item.style.height = `${sizes}px`;
+   item.style.width = `${sizes}px`;
+   sizes+=10;
+   item.style.backgroundColor = getRandomHexColor();
+   boxesDiv.append(item)
+  }
+}
+function destroyingHandler(){
+boxesDiv.innerHTML = ""
+sizes = 30;
+}
+createButton.addEventListener("click", creatingHandler)
+destroyButton.addEventListener("click", destroyingHandler)
+console.dir(boxesDiv)
